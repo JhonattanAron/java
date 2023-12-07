b6a884eed524f71aa456e9a0d4938809b159124f
Aron Cachago <115675860+JhonattanAron@users.noreply.github.com>
2023-12-07 14:07:28 -0500

    promediar

--- calculadora/Calculadora.java
+++ calculadora/Calculadora.java
@@ -2,9 +2,14 @@ public class Calculadora{
     public double Multiplicar(double x, double y){
         return x * y;
     }
-     public double Divide(double dividendo , double divisor){
+    public double Divide(double dividendo , double divisor){
         return dividendo / divisor;
-     }
+    }
+    public double promediar(double x, double y , double z){
+        double suma = x + y + z;
+        return suma / 3; 
+    }
+    
 
 
 }
