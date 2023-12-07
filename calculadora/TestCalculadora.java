574a611312a554c63a6dd44a49874bbabccc8a71
Aron Cachago <115675860+JhonattanAron@users.noreply.github.com>
2023-12-07 14:12:03 -0500

    testCalculadora

---
+++ calculadora/TestCalculadora.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        double resultadoMultiplicacion = calculadora.Multiplicar(5.0, 3.0);
+        System.out.println("Resultado de la multiplicación: " + resultadoMultiplicacion);
+
+        double resultadoDivision = calculadora.Divide(10.0, 2.0);
+        System.out.println("Resultado de la división: " + resultadoDivision);
+
+        double resultadoPromedio = calculadora.promediar(2.0, 4.0, 6.0);
+        System.out.println("Promedio de los números: " + resultadoPromedio);
+
+        calculadora.mostrarResultado();
+    }
+}
