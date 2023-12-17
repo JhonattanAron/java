533004f5525c4bfb8e8f9a7e3c1f12d7d2686991
Aron Cachago <115675860+JhonattanAron@users.noreply.github.com>
2023-12-17 14:02:47 -0500

    adminTelf

--- Taller1/Directorio/src/com/cmc/directorio/entidades/AdminTelefono.java
+++ Taller1/Directorio/src/com/cmc/directorio/entidades/AdminTelefono.java
@@ -1,5 +1,7 @@
 package com.cmc.directorio.entidades;
 
+import java.util.ArrayList;
+
 public class AdminTelefono {
 
     public void activarMensajeria(Telefono telefono) {
@@ -8,6 +10,17 @@ public class AdminTelefono {
         } else {
             System.out.println("El celular no tiene operadora");
         }
+    }
+
+    public String contarMovi(Telefono[] telefonos) {
+        ArrayList<String> list = new ArrayList<>();
+        for (Telefono telefono : telefonos) {
+            int i = 0;
+            if (telefono.getOperadora() == "movi") {
+                list.add(telefono.getOperadora());
+            }
+        }
+        return list.toString();
     }
 
 }
