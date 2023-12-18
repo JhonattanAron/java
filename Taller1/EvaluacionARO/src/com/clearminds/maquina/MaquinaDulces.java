57e001ec0733ce6e209e63c3f82ed772897879c5
Aron Cachago <115675860+JhonattanAron@users.noreply.github.com>
2023-12-18 15:22:15 -0500

    maquina 1.1

---
+++ Taller1/EvaluacionARO/src/com/clearminds/maquina/MaquinaDulces.java
@@ -0,0 +1,52 @@
+package com.clearminds.maquina;
+
+import com.clearminds.componentes.Celda;
+
+public class MaquinaDulces {
+    private Celda celda1;
+    private Celda celda2;
+    private Celda celda3;
+    private Celda celda4;
+    private double saldo;
+
+    public Celda getCelda1() {
+        return celda1;
+    }
+
+    public void setCelda1(Celda celda1) {
+        this.celda1 = celda1;
+    }
+
+    public Celda getCelda2() {
+        return celda2;
+    }
+
+    public void setCelda2(Celda celda2) {
+        this.celda2 = celda2;
+    }
+
+    public Celda getCelda3() {
+        return celda3;
+    }
+
+    public void setCelda3(Celda celda3) {
+        this.celda3 = celda3;
+    }
+
+    public Celda getCelda4() {
+        return celda4;
+    }
+
+    public void setCelda4(Celda celda4) {
+        this.celda4 = celda4;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+}
