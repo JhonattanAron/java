d0e52c2444a044e34a3342f9d7c90fb87c7137ab
Aron Cachago <115675860+JhonattanAron@users.noreply.github.com>
2023-12-18 15:18:57 -0500

    test celda

---
+++ Taller1/EvaluacionARO/src/com/clearminds/componentes/Producto.java
@@ -0,0 +1,50 @@
+package com.clearminds.componentes;
+
+/**
+ * Producto
+ */
+public class Producto {
+    private String nombre;
+    private double precio;
+    private String codigo;
+
+    public void incrementarPrecio(int porcentajeIncremento) {
+        double procentajeInc = (porcentajeIncremento / 100.0) * getPrecio();
+        setPrecio(procentajeInc + precio);
+    }
+
+    public Producto(String nombre, String codigo, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.codigo = codigo;
+    }
+
+    public void disminuirPrecio(double descuento) {
+        this.precio -= descuento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+}
